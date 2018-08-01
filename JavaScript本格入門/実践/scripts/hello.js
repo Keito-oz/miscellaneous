fd9e68f1cce007dbd31169d7c13e388e7149d0bd
KeitoOz <keito@workpc.com>
2018-08-01 15:08:37 +0900

    p106

--- JavaScript本格入門/実践/scripts/hello.js
+++ JavaScript本格入門/実践/scripts/hello.js
@@ -5,3 +5,20 @@ console.log(price*TAX);
 let name='Oz';
 let str=`Hi, ${name}-san, \n it is a good day!`;
 console.log(str);
+var x=15;
+if (x>10){
+    console.log('変数xは10より大きい');
+} else {
+    console.log('変数xは10以下です');
+}
+ var rank='C';
+ switch(rank){
+     case 'A':
+     case 'B':
+     case 'C':
+         console.log('合格');
+         break;
+    case 'D':
+        console.log('不合格');
+        break;
+ }
